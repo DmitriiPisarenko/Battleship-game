c74827d7c09552050ada942ca92c497c69d1d003
Dmitrii Pisarenko <pisarenko.d.y@gmail.com>
2020-06-03 21:33:03 +0600

    create field.

--- src/components/field/index.jsx
+++ src/components/field/index.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
+import propTypes from 'prop-types';
 import styles from './field.module.css';
+import Cell from '../cell';
 
-export default function Field() {
-  return(
-    <div className={styles.field} />
-  )
+export default function Field(props) {
+  const { field } = props;
+  return (
+    <div className={styles.field}>
+      <div className={styles.cells}>
+        {field.map((row, i) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <div key={i} className={styles.row}>
+            {row.map((cell, j) => (
+              // eslint-disable-next-line react/no-array-index-key
+              <Cell key={j} row={i} col={j}>
+                {cell}
+              </Cell>
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
+
+Field.propTypes = {
+  field: propTypes.arrayOf(
+    propTypes.arrayOf(
+      propTypes.string,
+    ),
+  ).isRequired,
+};
--- src/components/host/index.jsx
+++ src/components/host/index.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
-import styles from './host.module.css'
+import React, { useState, useEffect } from 'react';
+import styles from './host.module.css';
+import Field from '../field';
 
 export default function Host() {
-  return(
-    <div className={styles.container} />
-    
-  )
+  const [field, updateField] = useState([]);
+  useEffect(() => {
+    const newField = new Array(11).fill(undefined).map(() => new Array(11).fill(undefined));
+    updateField(newField);
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <div>
+        <Field field={field} />
+        <Field field={field} />
+      </div>
+      <div>
+        <Field field={field} />
+        <Field field={field} />
+      </div>
+    </div>
+  );
 }
